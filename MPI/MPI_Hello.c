8f0dcb4bc5b6f35bc4d8c67efabd4d8e4d51949c
Daniil Stefonishin <stefonishin@gmail.com>
2019-04-02 10:21:48 +0300

    Simplest program

---
+++ MPI/MPI_Hello.c
@@ -0,0 +1,32 @@
+# include <mpi.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+/// Simplest program
+/// Compile with 'mpicc MPI_Hello.c -o hello'
+/// Run with 'mpirun -n <process_number> hello'
+
+int main(int argc, char ** argv)
+{
+    int psize;
+    int prank;
+    MPI_Status status;
+
+    int ierr;
+
+    ierr = MPI_Init(&argc, &argv);
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &prank);
+    ierr = MPI_Comm_size(MPI_COMM_WORLD, &psize);
+
+    // root process
+    if (prank == 0)
+    {
+        printf("The number of processes available is %d\n", psize);
+    }
+
+    printf("Hello world from process[%d]\n", prank);
+
+    ierr = MPI_Finalize();
+
+    return 0;
+}
